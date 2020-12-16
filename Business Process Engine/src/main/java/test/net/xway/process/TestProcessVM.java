8018b6ae5319fc52995d8810a35c782174992fc9
vcshcn <vcshcn@github.com>
2020-12-16 23:00:17 +0800

    update

---
+++ Business Process Engine/src/main/java/test/net/xway/process/TestProcessVM.java
@@ -0,0 +1,14 @@
+package test.net.xway.process;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestProcessVM {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
